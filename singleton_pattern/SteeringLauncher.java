a54e2851a296a010fd184612cddb078aa9912bc7
subbus <subbareddyg83@gmail.com>
2020-11-12 15:38:28 +0530

    added singleton pattern

---
+++ singleton_pattern/SteeringLauncher.java
@@ -0,0 +1,46 @@
+package singleton_pattern;
+
+class Steering 
+{
+    private String type;
+    private static Steering uniqueSteering;
+
+    private Steering() 
+    {
+        type = "";
+    }
+    public void setType(String type) 
+    {
+        this.type = type;
+    }
+    public String getType()
+    {
+        return type;
+    }
+
+    public static Steering getUniqueSteering() 
+    {
+        if (uniqueSteering == null) 
+        {
+            uniqueSteering = new Steering();
+        }
+        return uniqueSteering;
+    }
+}
+
+public class SteeringLauncher 
+{
+    public static void main(String[] args) 
+    {
+        //each variable aceess the same object
+        //hence they  display same object hashcode
+        Steering steering_1 = Steering.getUniqueSteering();
+        Steering steering_2 = Steering.getUniqueSteering();
+        Steering steering_3 = Steering.getUniqueSteering();
+        System.out.println("steering_1 is " + steering_1);
+        System.out.println("steering_2 is " + steering_2);
+        System.out.println("steering_3 is " + steering_3);
+        steering_1.setType("power-steering");
+        System.out.println(steering_2.getType());
+    }
+}
