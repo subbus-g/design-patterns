38d8f8c07b452cfc528f48cc802bc992364cb5ce
subbus <subbareddyg83@gmail.com>
2020-11-16 11:29:34 +0530

    add factory method definition

--- factory_method_pattern/User.java
+++ factory_method_pattern/User.java
@@ -1,5 +1,10 @@
 package factory_method_pattern;
-
+/*
+//-----definition-----
+//Define an interface for creating an object,
+//but let subclasses decide which class to instantiate.
+//Factory Method lets a class defer instantiation to subclasses.
+*/
 public class User 
 {
     public static void main(String[] args)
